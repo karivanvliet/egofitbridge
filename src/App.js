4f7240061affebdbf8f9546e75c4f6daeb2ce600
karivanvliet <karivanv@gmail.com>
2023-12-17 15:37:36 -0500

    bluetooth functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Connect, Request } from './components/Bluetooth.js'
+import Auth from './components/Auth.js'
+import { useState } from 'react';
 
 function App() {
+  const [service, setService] = useState()
+
+  const handleServiceChange = (newService) => {
+    setService(newService)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Auth />
+      <Connect handleServiceChange={handleServiceChange} />
+      {service ? <Request service={service} /> : <></>}
     </div>
   );
 }
---
+++ src/components/Auth.js
@@ -0,0 +1,12 @@
+function Auth() {
+    return (
+        <div id="g_id_onload"
+            data-client_id="578097828784-l0ieit344qhi27fas9q2gp6i3fn3smjd.apps.googleusercontent.com"
+            data-login_uri="http://localhost:3000"
+            data-your_own_param_1_to_login="any_value"
+            data-your_own_param_2_to_login="any_value">
+        </div>
+    )
+}
+
+export default Auth;
