8ecde222ccad7d14db04f98a53328c8f65cae432
karivanvliet <karivanv@gmail.com>
2024-01-28 12:55:19 -0500

    fix step parsing bug

--- src/Utility/parseData.js
+++ src/Utility/parseData.js
@@ -8,10 +8,9 @@ function parseData(rawData) {
   if (valueArray.length === 17) {
     newTreadmillData.speed = parseInt(valueArray[3], 16) / 10;
     newTreadmillData.distance = parseInt(valueArray[8] + valueArray[7], 16) / 1000;
-
     newTreadmillData.calories = parseInt(valueArray[10] + valueArray[9], 16) / 10;
     newTreadmillData.sessionTimeSec = parseInt(valueArray[6] + valueArray[5], 16);
-    newTreadmillData.steps = parseInt(valueArray[11], 16);
+    newTreadmillData.steps = parseInt(valueArray[12] + valueArray[11], 16);
   }
 
   return newTreadmillData;
